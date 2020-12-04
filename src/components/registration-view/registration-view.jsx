ab37904b2033c277041566f96ed679e85a9daffb
MitchellZilberg <64373649+MitchellZilberg@users.noreply.github.com>
2020-12-04 13:45:43 -0500

    added register component

---
+++ src/components/registration-view/registration-view.jsx
@@ -0,0 +1,36 @@
+import React, {useState} from 'react'; 
+
+export function RegistrationView(props) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [birthday, setBirthday] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); 
+    console.log(username, password, email, birthday);
+    props.onLoggedIn(username);
+  }
+
+  return (
+    <form>
+      <label >Username: 
+        <input type="text" value={username} onChange= {e => 
+        setUsername(e.target.value)}/>
+      </label>
+      <label >Password: 
+        <input type="text" value={password} onChange= {e => 
+        setPassword(e.target.value)}/>
+      </label>
+      <label >Email: 
+        <input type="email" value={email} onChange= {e => 
+        setEmail(e.target.value)}/>
+      </label>
+      <label >Birthday: 
+        <input type="text" value={birthday} onChange= {e => 
+        setBirthday(e.target.value)}/>
+      </label>
+      <button type='button' onClick={handleSubmit}>Submit</button>
+    </form>
+  )
+}
