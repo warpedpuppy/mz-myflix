479a228c3393abcc42c08431b115d2d3ca432a95
MitchellZilberg <64373649+MitchellZilberg@users.noreply.github.com>
2020-12-07 12:23:26 -0500

    removed form

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -17,29 +17,7 @@ export function RegistrationView(props) {
   }
 
   return (
-//     <form>
-//       <label >Username: 
-//         <input type="text" value={username} onChange= {e => 
-//         setUsername(e.target.value)}/>
-//       </label>
-//       <label >Password: 
-//         <input type="text" value={password} onChange= {e => 
-//         setPassword(e.target.value)}/>
-//       </label>
-//       <label >Email: 
-//         <input type="email" value={email} onChange= {e => 
-//         setEmail(e.target.value)}/>
-//       </label>
-//       <label >Birthday: 
-//         <input type="date" value={birthday} onChange= {e => 
-//         setBirthday(e.target.value)}/>
-//       </label>
-//       <button type='button' onClick={handleSubmit}>Submit</button>
-//     </form>
-//   )
-// }
-
-<Form className='login-form'>
+    <Form className='login-form'>
       <FormGroup>
         <label>
           <input type='text' value={username} placeholder='Username' onChange = {e => 
