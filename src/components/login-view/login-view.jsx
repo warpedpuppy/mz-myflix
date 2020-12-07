b76a8ab0a015e1e727ed86ed3ad13da7c1fa4dcf
MitchellZilberg <64373649+MitchellZilberg@users.noreply.github.com>
2020-12-07 12:23:19 -0500

    added authentication

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,11 +1,10 @@
 import React, {useState} from 'react'; 
-import PropTypes from 'prop-types';
+import axios from 'axios';
 import{RegistrationView} from '../registration-view/registration-view';
 import { 
-  Container, Row, Col, Form, FormGroup, Label, Input
+  Button, Container, Row, Col, Form, FormGroup, Label, Input
 } from 'react-bootstrap';
 
-import Button from 'react-bootstrap/Button';
 
 export function LoginView(props) {
   const [ username, setUsername ] = useState(''); 
@@ -14,23 +13,36 @@ export function LoginView(props) {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(username, password);
-     // Send a request to the server for authentication then call props.onLoggedIn(username) 
-     props.onLoggedIn(username);
+    
+    // Send a request to the server for authentication
+    axios.post('https://radiant-journey-16913.herokuapp.com/login', {
+      Username: username,
+      Password: password
+
+      // props.onLoggedIn(username)
+    })
+    .then (response => {
+      const data = response.data;
+      props.onLoggedIn(data);
+    })
+    .catch(e => {
+      console.log('user doesnt exist')
+    });    
   };
 
   return (
     <Form className='login-form'>
-      <FormGroup>
-        <label>
-          <input className='user-input' type='text' value={username} placeholder='Username' onChange = {e => 
+      <FormGroup controlId='FormBasicUsername'>
+        <Form.Label>
+          <Form.Control className='user-input' type='text' value={username} placeholder='Username' onChange = {e => 
           setUsername(e.target.value)} />
-        </label>
+        </Form.Label>
       </FormGroup>
-      <FormGroup>
-        <label>
-          <input className='password-input' type='password' value={password} placeholder='Password' onChange = {e => 
+      <FormGroup controlId='FormBasicPassword'>
+        <Form.Label>
+          <Form.Control className='password-input' type='password' value={password} placeholder='Password' onChange = {e => 
           setPassword(e.target.value)} />
-        </label>
+        </Form.Label>
       </FormGroup>
       <FormGroup>
         <Button className='login-button' type='button' onClick={handleSubmit}>LOGIN</Button>
