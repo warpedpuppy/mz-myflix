15e98806dd75eeb2125284d450a98bb8b253b195
MitchellZilberg <64373649+MitchellZilberg@users.noreply.github.com>
2020-12-23 09:54:48 -0500

    changed to remote

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-  // API_URL: 'https://radiant-journey-16913.herokuapp.com',
-  API_URL: 'http://localhost:8080'
+  API_URL: 'https://radiant-journey-16913.herokuapp.com',
+  // API_URL: 'http://localhost:8080'
 }
 
 
